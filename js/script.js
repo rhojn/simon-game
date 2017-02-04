e49a02f6f6ce66c0ceb8ad96cf0b1a13b51b05b3
rhojn <rhojeno8@gmail.com>
2017-02-04 17:40:06 +0800

    edit error

--- js/script.js
+++ js/script.js
@@ -35,7 +35,7 @@ var effect1 = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3")
 var effect2 = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound2.mp3");
 var effect3 = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound3.mp3");
 var effect4 = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound4.mp3");
-var error = new Audio("sounds/error.mp3");
+var error = new Audio("https://raw.githubusercontent.com/rhojn/simon-game/master/sounds/Error.mp3");
 
 function random(min, max) {
     return Math.floor((Math.random()*4)+1);
